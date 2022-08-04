07f775ec6a76928615a52a65807ca78e2d796aa0
John Billy Z. Fruto <frutobilly@gmail.com>
2022-08-04 11:30:07 +0800

    Created get requests for gender and user types.

--- routes/api.py
+++ routes/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
-from routes.endpoints import (user, login, employer)
+from routes.endpoints import (employer, gender, login, user, user_type)
 
 router = APIRouter()
 
 router.include_router(login.router)
+router.include_router(user_type.router)
+router.include_router(gender.router)
 router.include_router(user.router)
 router.include_router(employer.router)
