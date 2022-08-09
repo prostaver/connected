56fb45f0f673598640b6c83dfa9251f9a4ed90fb
John Billy Z. Fruto <frutobilly@gmail.com>
2022-08-09 15:45:14 +0800

    Created routes, schemas, and service logic for job position

---
+++ pydantic_schemas/job_position.py
@@ -0,0 +1,21 @@
+from decimal import Decimal
+
+from pydantic import BaseModel
+from pydantic_schemas.employer import Employer
+
+class BaseJobPosition(BaseModel):
+    title: str
+    description: str
+    salary: Decimal
+
+class CreateJobPosition(BaseJobPosition):
+    pass
+
+class JobPosition(BaseJobPosition):
+    id: int
+    employer_id: int
+
+    employer: Employer
+
+    class Config:
+        orm_mode = True
--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from routes.endpoints import (applicant, employer, gender, login, user, user_type)
+from routes.endpoints import (applicant, employer, employment_log, gender, job_position, login, user, user_type)
 
 router = APIRouter()
 
@@ -8,4 +8,6 @@ router.include_router(user_type.router)
 router.include_router(gender.router)
 router.include_router(user.router)
 router.include_router(employer.router)
+router.include_router(job_position.router)
 router.include_router(applicant.router)
+#router.include_router(employment_log.router)
